b3fe36a76d98a7ec34671f5c393cdce5e344267c
sspread <sspread2@gmail.com>
2014-10-03 14:11:21 -0500

    Implemented deleting and error handling

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,14 +4,21 @@ end
 
 get '/u/:url' do
   @upload = Upload.find_by_url(params[:url])
+  # ajax message
   erb :download
 end
+
+delete '/u' do
+  Upload.find_by_url(params[:url]).destroy
+end
+
 
 post '/upload' do
   url = url_generator
   until Upload.find_by(url: url) == nil
     url = url_generator
   end
+  params[:upload][:filename] = url
   Upload.create(filepath: params[:upload], url: url)
   url
 end
